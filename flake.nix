91f1aff6cd4860d32441acfcb043284641ef7f90
simidzu2ay <simidzu2ay@protonmail.com>
2025-04-02 12:06:28 +0200

    dev shell for aarch64

--- flake.nix
+++ flake.nix
@@ -8,7 +8,11 @@
     nixpkgs,
   }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    pkgs-aarch = nixpkgs.legacyPackages."aarch64-linux";
   in {
     devShells."x86_64-linux".default = import ./shell.nix {inherit pkgs;};
+    devShells."aarch64-linux".default = import ./shell.nix {
+      pkgs = pkgs-aarch;
+    };
   };
 }
