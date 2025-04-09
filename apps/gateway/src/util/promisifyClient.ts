e32fa3f9b64765948a8c25f3ced54e6db5082123
henmalib <simidzu2ay@protonmail.com>
2025-04-09 14:57:42 +0200

    apps/gateway: better grpc client

--- apps/gateway/src/util/promisifyClient.ts
+++ apps/gateway/src/util/promisifyClient.ts
@@ -1,4 +1,4 @@
-// Fully copied from:
+// Mostly copied from:
 // https://github.com/timostamm/protobuf-ts/discussions/345#discussioncomment-3106495
 
 import type {
@@ -22,6 +22,17 @@ type PromisifiedCall<T, U> = (
 	options?: Partial<CallOptions>,
 ) => Promise<U>;
 
+interface GRPCError {
+	code: number;
+	message: string;
+}
+
+type SafePromisifiedCall<T, U> = (
+	request: T,
+	metadata?: Metadata,
+	options?: Partial<CallOptions>,
+) => Promise<[GRPCError, U]>;
+
 export type PromisifiedClient<C> = { $: C } & {
 	[prop in Exclude<keyof C, keyof Client>]: C[prop] extends OriginalCall<
 		infer T,
@@ -31,17 +42,37 @@ export type PromisifiedClient<C> = { $: C } & {
 		: never;
 };
 
+export type SafeClient<C> = {
+	[prop in Exclude<
+		keyof PromisifiedClient<C>,
+		symbol
+	> as `${prop}Safe`]: PromisifiedClient<C>[prop] extends PromisifiedCall<
+		infer T,
+		infer U
+	>
+		? SafePromisifiedCall<T, U>
+		: never;
+};
+
+export type BetterClient<C> = SafeClient<C> & PromisifiedClient<C>;
+
 export function promisifyClient<C extends Client>(client: C) {
 	return new Proxy(client, {
 		get: (target, descriptor) => {
-			const key = descriptor as keyof PromisifiedClient<C>;
+			const ds = descriptor.toString();
+
+			const isSafe = ds.endsWith('Safe');
+
+			const key = (
+				isSafe ? ds.slice(0, -4) : descriptor
+			) as keyof PromisifiedClient<C>;
 
 			if (key === '$') return target;
 
 			const func = target[key];
 			if (typeof func === 'function')
-				return (...args: unknown[]) =>
-					new Promise((resolve, reject) =>
+				return (...args: unknown[]) => {
+					const promise = new Promise((resolve, reject) =>
 						func.call(
 							target,
 							...[
@@ -51,6 +82,15 @@ export function promisifyClient<C extends Client>(client: C) {
 							],
 						),
 					);
+
+					if (!isSafe) return promise;
+
+					return new Promise((resolve) => {
+						promise
+							.then((r) => resolve([null, r]))
+							.catch((e) => resolve([e, null]));
+					});
+				};
 		},
-	}) as unknown as PromisifiedClient<C>;
+	}) as unknown as BetterClient<C>;
 }
