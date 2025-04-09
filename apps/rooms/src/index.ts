ad8ec940f0ab605559ac758b9e1dfb2e52af3b0e
henmalib <simidzu2ay@protonmail.com>
2025-04-09 19:03:35 +0200

    Auto-add license

--- apps/rooms/src/index.ts
+++ apps/rooms/src/index.ts
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2025  henmalib
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as grpc from '@grpc/grpc-js';
 import { type IRoomsServer, RoomsService } from 'grpc/rooms/v1/rooms_grpc_pb';
 import { GetRoomsResponse, Room } from 'grpc/rooms/v1/rooms_pb';
